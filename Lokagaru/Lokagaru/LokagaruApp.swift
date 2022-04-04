a7318356b78f67b590dac4b67983a07ccc531535
hunter <85328038+huntercodes@users.noreply.github.com>
2022-04-04 03:20:04 -0500

    Add files via upload

---
+++ Lokagaru/Lokagaru/LokagaruApp.swift
@@ -0,0 +1,17 @@
+//
+//  LokagaruApp.swift
+//  Lokagaru
+//
+//  Created by hunter downey on 3/21/22.
+//
+
+import SwiftUI
+
+@main
+struct LokagaruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TunerView()
+        }
+    }
+}
---
+++ Lokagaru/Lokagaru/Views/TranspositionMenu.swift
@@ -0,0 +1,59 @@
+//
+//  TranspositionMenu.swift
+//  Lokagaru
+//
+//  Created by hunter downey on 4/4/22.
+//
+
+import SwiftUI
+
+struct TranspositionMenu: View {
+    private let transpositions = ScaleNote.allCases.map(\.transpositionName)
+    @Binding var selectedTransposition: Int
+
+    var body: some View {
+        Menu(
+            content: {
+                ForEach(0..<transpositions.count, id: \.self) { index in
+                    Button(
+                        action: {
+                            selectedTransposition = index
+                        },
+                        label: {
+                            Text(transpositions[index])
+                        }
+                    )
+                }
+            },
+            label: {
+                Text(transpositions[selectedTransposition])
+                    // Increase tap area, some of the transpositions are just a single
+                    // letter so the tap area can otherwise be quite small.
+                    .frame(minWidth: 100, alignment: .leading)
+            }
+        )
+        .transaction { transaction in
+            // Disable jarring animation when menu label changes width
+            transaction.disablesAnimations = true
+            transaction.animation = nil
+        }
+    }
+}
+
+private extension ScaleNote {
+    var transpositionName: String {
+        switch self {
+        case .C:
+            return "Concert Pitch"
+        default:
+            return names.joined(separator: " / ")
+        }
+    }
+}
+
+struct TranspositionMenu_Previews: PreviewProvider {
+    static var previews: some View {
+        TranspositionMenu(selectedTransposition: .constant(0))
+            .previewLayout(.sizeThatFits)
+    }
+}
